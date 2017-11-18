65c1e0269f04978c3306c3131ff462ce0f5c93de
ntnikhilesh <ntnikhilesh@gmail.com>
2017-11-18 22:17:32 +0530

    component , variable, object , array , data binding , event

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  fname="Nikhil";
+  lname:string;
+  age:number;
+  //object type1
+  address:{
+    city:string;
+    state:string
   }
 
+  //object type2
+  address1:Address1;
+
+  //Array
+  hobbies:string[];
+
+
+
+  constructor() { 
+    console.log("constructor ran");
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit ran");
+
+    this.lname="Tripathi";
+    this.age=23;
+
+    this.address={
+      city:'BLR',
+      state:'KK'
+    }
+
+    this.address1={
+      city:'BLR1',
+      state:'KK1'
+    }
+    this.hobbies=['Cricket', 'Net surfiing']
+
+  }
+
+  clickMe(){
+    this.hobbies.push('New Hobby')
+  }
+
+}
+
+
+
+interface Address1{
+  city:string;
+  state:string;
 }
