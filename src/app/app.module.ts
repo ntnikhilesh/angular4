6c73eae0b530617a0f5779adb12dda5581432bb8
ntnikhilesh <ntnikhilesh@gmail.com>
2017-11-18 23:49:54 +0530

    2 way data binding with fname

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { UserComponent } from './components/user/user.component';
   ],
   imports: [
     BrowserModule,
-    //FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
